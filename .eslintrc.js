56a6d24fede5c3354271d0efbd041dd0bef807cf
magic-zhu <18817313320@163.com>
2022-04-15 18:38:10 +0800

    :up:

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   'plugins': [
     '@typescript-eslint',
   ],
-  'rules': {},
+  'rules': {
+    'linebreak-style': 'off',
+  },
 };
---
+++ src/core/Line.ts
@@ -0,0 +1,21 @@
+import BaseNode from './BaseNode';
+
+/**
+ *
+ *
+ * @class Line
+ * @extends {BaseNode}
+ */
+class Line extends BaseNode {
+    start: number[] = [0, 0]
+    end: number[] = [0, 0]
+    /**
+       * Creates an instance of Line.
+       * @memberof Line
+       */
+    constructor() {
+
+    }
+}
+
+export default Line;
